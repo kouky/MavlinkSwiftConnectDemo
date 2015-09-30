26aafb898d643d1acbb732a104aa5e9cbf3e794a
Michael Koukoullis <mike@kouky.org>
2015-09-30 22:58:25 +1000

    Initial Mac application

---
+++ Sources/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  MavlinkSwiftConnectDemo
+//
+//  Created by Michael Koukoullis on 30/09/2015.
+//  Copyright © 2015 Michael Koukoullis. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+	@IBOutlet weak var window: NSWindow!
+
+
+	func applicationDidFinishLaunching(aNotification: NSNotification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(aNotification: NSNotification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
