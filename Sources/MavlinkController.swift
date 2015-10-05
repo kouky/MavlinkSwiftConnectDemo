2efeac93ddecac7efd24bfe587e3d3194b62aa9e
Michael Koukoullis <mike@kouky.org>
2015-10-05 22:31:14 +1100

    MavlinkController class

---
+++ Sources/MavlinkController.swift
@@ -0,0 +1,15 @@
+//
+//  MavlinkController.swift
+//  MavlinkSwiftConnectDemo
+//
+//  Created by Michael Koukoullis on 5/10/2015.
+//  Copyright © 2015 Michael Koukoullis. All rights reserved.
+//
+
+import Cocoa
+import ORSSerial
+
+class MavlinkController: NSObject {
+	
+	let serialPortManager = ORSSerialPortManager.sharedSerialPortManager()
+}
