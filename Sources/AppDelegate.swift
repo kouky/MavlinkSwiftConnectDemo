be49f2e0bb096ea066e4c498ceec16838607a258
Michael Koukoullis <mike@kouky.org>
2015-10-05 21:58:15 +1100

    Nuke AppDelegate boilerplate

--- Sources/AppDelegate.swift
+++ Sources/AppDelegate.swift
@@ -12,16 +12,4 @@ import Cocoa
 class AppDelegate: NSObject, NSApplicationDelegate {
 
 	@IBOutlet weak var window: NSWindow!
-
-
-	func applicationDidFinishLaunching(aNotification: NSNotification) {
-		// Insert code here to initialize your application
-	}
-
-	func applicationWillTerminate(aNotification: NSNotification) {
-		// Insert code here to tear down your application
-	}
-
-
 }
-
