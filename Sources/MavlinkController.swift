227011d02f4ed46f04278992ecbcbd739469b7f7
Michael Koukoullis <mike@kouky.org>
2015-10-06 20:53:56 +1100

    Start Mavlink on open button press

--- Sources/MavlinkController.swift
+++ Sources/MavlinkController.swift
@@ -26,11 +26,32 @@ class MavlinkController: NSObject, ORSSerialPortDelegate {
     // MARK: Actions
 
     @IBAction func openOrClosePort(sender: AnyObject) {
-        print("Button press")
+        if let port = serialPort {
+            if (port.open) {
+                port.close()
+            }
+            else {
+    			port.baudRate = 57600
+    			port.numberOfStopBits = 1
+    			port.parity = .None
+                port.open()
+    			if let data = "mavlink start -d /dev/ttyACM0\n".dataUsingEncoding(NSUTF32LittleEndianStringEncoding) {
+    				port.sendData(data)
+    			}
+            }
+        }
     }
 
     // MARK: ORSSerialPortDelegate Protocol
 
+    func serialPortWasOpened(serialPort: ORSSerialPort) {
+        self.openCloseButton.title = "Close"
+    }
+    
+    func serialPortWasClosed(serialPort: ORSSerialPort) {
+        self.openCloseButton.title = "Open"
+    }
+    
     func serialPortWasRemovedFromSystem(serialPort: ORSSerialPort) {
         self.serialPort = nil
         self.openCloseButton.title = "Open"
